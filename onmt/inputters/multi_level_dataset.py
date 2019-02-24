1dbb9f83787b0e3d4749727930be25dab9356fdc
Itay Sofer <ItaySofer@users.noreply.github.com>
2019-02-24 19:25:03 +0200

    multi-level data preprocessing

---
+++ onmt/inputters/multi_level_dataset.py
@@ -0,0 +1,21 @@
+import onmt.inputters
+
+
+class MultiLevelDataset(onmt.inputters.Dataset):
+    """
+    A single language tweeter dataset.
+    """
+
+    def __init__(self, fields, readers, data, dirs, sort_key, level,
+                 filter_pred=None):
+        super(MultiLevelDataset, self).__init__(fields, readers, data, dirs, sort_key, filter_pred)
+        self.level = level
+
+    def __getitem__(self, idx):
+        example = super(MultiLevelDataset, self).__getitem__(idx)
+        example.levels = self.level
+        return example
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self.__getitem__(i)
