5afff367a12319ac8c2aee9e2e59b53260917ab7
Itay Sofer <ItaySofer@users.noreply.github.com>
2019-02-26 19:45:26 +0200

    minor change

--- onmt/inputters/__init__.py
+++ onmt/inputters/__init__.py
@@ -7,6 +7,7 @@ from onmt.inputters.inputter import \
     load_old_vocab, get_fields, OrderedIterator, \
     build_vocab, old_style_vocab, filter_example
 from onmt.inputters.dataset_base import Dataset
+from onmt.inputters.multi_level_dataset import MultiLevelDataset
 from onmt.inputters.text_dataset import text_sort_key, TextDataReader
 from onmt.inputters.image_dataset import img_sort_key, ImageDataReader
 from onmt.inputters.audio_dataset import audio_sort_key, AudioDataReader
@@ -19,7 +20,7 @@ str2sortkey = {
     'text': text_sort_key, 'img': img_sort_key, 'audio': audio_sort_key}
 
 
-__all__ = ['Dataset', 'load_old_vocab', 'get_fields', 'DataReaderBase',
+__all__ = ['Dataset', 'MultiLevelDataset', 'load_old_vocab', 'get_fields', 'DataReaderBase',
            'filter_example', 'old_style_vocab',
            'build_vocab', 'OrderedIterator',
            'text_sort_key', 'img_sort_key', 'audio_sort_key',
